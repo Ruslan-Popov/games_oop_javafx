10840a5ac352ff35eb678a7d8a70e67d78f98276
Ruslan-Popov <ruslan_popov@mail.ru>
2020-12-05 01:38:02 +0300

    1.2.6.2. Каркас шахматной доски [#288498]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,15 +4,36 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.RookBlack;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class LogicTest {
 
     @Ignore
-    @Test
-    public void move()
+    @Test(expected = FigureNotFoundException.class)
+    public void whenCellEmpty()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.D2, Cell.H6);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenFigureOnTheWay() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new RookBlack(Cell.D2));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test
+    public void whenMove() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        BishopBlack slon = new BishopBlack(Cell.C1);
+        logic.add(slon);
+        logic.move(Cell.C1, Cell.H6);
+        Cell rst = slon.position();
+        assertThat(rst, is(Cell.H6));
     }
 }
